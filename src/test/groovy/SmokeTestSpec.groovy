2f64a1432619d3978f484abec49346d972c6defa
Iain Adams <iwarapter@hotmail.com>
2014-06-14 21:19:57 +0100

    Tested running specific tests after reading: http://mrhaki.blogspot.co.uk/2013/05/gradle-goodness-running-single-test.html
    gradlew clean chromeTest -DchromeTest.single=Smoke*
    Can be used to run only the tests starting with Smoke, added the test logging for command line verification.

---
+++ src/test/groovy/SmokeTestSpec.groovy
@@ -0,0 +1,33 @@
+import spock.lang.*
+import geb.*
+import geb.spock.*
+
+class SmokeTestSpec extends GebReportingSpec {
+
+    def "Check the login page loads"() {
+        when: "we are at the login page"
+        to LoginPage
+
+        then: "the page has loaded"
+        at LoginPage
+    }
+
+    def "Check we can login"() {
+        when: "we are at the login page"
+        to LoginPage
+
+        then: "the page has loaded"
+        at LoginPage
+
+        when: "we add our user details"
+        username = 'user1'
+        password = 'password1'
+        
+        and: "we click login"
+        loginButton.click()
+
+        then: "We are taken to the correct page"
+        at CheckLoginPage
+    }
+
+}
