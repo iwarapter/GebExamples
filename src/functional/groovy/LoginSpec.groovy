90306786bac1a7c93f1bdeea521f7ad4c236fcd5
Iain Adams <iwarapter@hotmail.com>
2014-06-22 23:54:46 +0100

    Added an example data table test.

--- src/functional/groovy/LoginSpec.groovy
+++ src/functional/groovy/LoginSpec.groovy
@@ -22,4 +22,27 @@ class LoginSpec extends GebReportingSpec {
         at CheckLoginPage
     }
 
+    @Unroll
+    def "Try login with #user"(){
+        when: "we are at the login page"
+        to LoginPage
+
+        then: "the page has loaded"
+        waitFor { at LoginPage }
+
+        when: "we add our user details"
+        username = user
+        password = pass
+
+        and:
+        loginButton.click()
+
+        then: "We are taken to the correct page"
+        at CheckLoginPage
+
+        where:
+        user    | pass
+        'user1' | 'password1'
+        'user2' | 'password2'
+    }
 }
